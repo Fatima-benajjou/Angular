ecc7720a207f92487eea1698cb26d573b4ef2e6f
Fatima BENAJJOU <fatima.benajjou@gmail.com>
2024-09-04 16:56:41 +0200

    exo pokemon

---
+++ exercice6/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+
+import { PokemonComponent } from './pages/pokemon/pokemon.component';
+
+export const routes: Routes = [
+  
+  {path: 'pokemon', component: PokemonComponent},
+];
+
---
+++ exercice6/src/app/pages/pokemon/pokemon.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-pokemon',
+  standalone: true,
+  imports: [ FormsModule,
+    ReactiveFormsModule],
+  templateUrl: './pokemon.component.html',
+  styleUrl: './pokemon.component.css'
+})
+
+// form reactive //
+
+
+export class PokemonComponent {
+  pokemon: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.pokemon = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      types: this.fb.array([], Validators.required),
+      attacks: this.fb.array([this.createAttackGroup()]),
+      zone: this.fb.group({
+        name: ['', Validators.required],
+        region: ['', Validators.required]
+      })
+    });
+  }
+
+  createAttackGroup(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      damage: ['', Validators.required]
+    });
+  }
+
+  get types(): FormArray {
+    return this.pokemon.get('types') as FormArray;
+  }
+
+  get attacks(): FormArray {
+    return this.pokemon.get('attacks') as FormArray;
+  }
+
+  addType(type: string): void {
+    this.types.push(this.fb.control(type));
+  }
+
+  addAttack(): void {
+    this.attacks.push(this.createAttackGroup());
+  }
+
+  removeAttack(index: number): void {
+    this.attacks.removeAt(index);
+  }
+
+  onSubmit(): void {
+    if (this.pokemon.valid) {
+      console.log(this.pokemon.value);
+    
+    }
+  }
+}
